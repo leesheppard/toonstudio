bf3e39c1e2a88e774ebb39bf870a87650b58fe64
Lee Sheppard <lee@leesheppard.com>
2020-12-04 21:34:02 +1100

    Ride the lobster

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="profile"></div>
+      <div className="hud"></div>
+      <div className="main">
+        <div className="framer">
+          <img src="https://res.cloudinary.com/leesheppard/image/upload/v1595069548/toonstudio_logo_scspcu.svg" className="App-logo" alt="Toon Studio logo" /><br />
+            <h5>Traditional 2D artwork and Application development</h5>  
+            <img src="https://res.cloudinary.com/leesheppard/image/upload/v1596000875/artwork/hiro_white.svg" width="200px" alt="Hiro character illustration" />
+              <footer>
+              <a href="https://dribbble.com/leesheppard" ><i className="fab fa-dribbble"></i></a>
+              <a href="https://github.com/leesheppard" ><i className="fab fa-github-alt"></i></a>
+              <a href="https://facebook.com/leesheppard" ><i className="fab fa-facebook-f"></i></a>
+              <a href="https://twitter.com/leesheppard" ><i className="fab fa-twitter"></i></a>
+              <a href="https://www.linkedin.com/in/leesheppard" ><i className="fab fa-linkedin-in"></i></a>
+              </footer>
+        </div>
+      </div>
     </div>
   );
 }
